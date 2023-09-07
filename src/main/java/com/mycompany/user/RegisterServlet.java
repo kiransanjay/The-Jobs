3ee5c38cf47777014c779135ea986b919f175c26
kiransanjay <115564119+kiransanjay@users.noreply.github.com>
2023-09-07 19:57:31 +0530

    Register success message added along with design changes

---
+++ src/main/java/com/mycompany/user/RegisterServlet.java
@@ -0,0 +1,27 @@
+package com.mycompany.user;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet
+public class RegisterServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String firstName = request.getParameter("first_name");
+        String lastName = request.getParameter("last_name");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        User newUser = new User();
+        newUser.setFirstName(firstName);
+        newUser.setLastName(lastName);
+        newUser.setEmail(email);
+        newUser.setPassword(password);
+
+        response.sendRedirect("registerSuccess.html");
+    }
+}
