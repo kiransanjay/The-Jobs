66cd20a7ddb7d2f4c1fe4acc12b7083b6d203168
kiransanjay <115564119+kiransanjay@users.noreply.github.com>
2023-09-16 14:59:05 +0530

    New Pages Added

--- src/main/java/com/mycompany/controller/MainController.java
+++ src/main/java/com/mycompany/controller/MainController.java
@@ -5,10 +5,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-    @GetMapping("")
+
+    @GetMapping("/index.html")
     public String showHomePage() {
-//        System.out.println("main controller");
         return "index";
     }
+
+    @GetMapping("/loginPage.html")
+    public String showLoginPage() {
+        return "loginPage";
+    }
+
+    @GetMapping("/jobPage.html")
+    public String showJobsPage() {
+        return "jobPage";
+    }
+
+    @GetMapping("/createUser.html")
+    public String showCreateUserPage() {
+        return "createUser";
+    }
+
+    @GetMapping("/consultantPage.html")
+    public String showConsultantPage() {
+        return "consultantPage";
+    }
+
+    @GetMapping("/profile.html")
+    public String showProfilePage() {
+        return "profile";
+    }
+
+
 }
 
